8c1cb56aef394ca29bb37e949ab3de7849eb8554
skohar <skohar@users.noreply.github.com>
2015-08-29 17:51:30 +0900

    .

--- src/main/scala/com/github/skohar/homeip/App.scala
+++ src/main/scala/com/github/skohar/homeip/App.scala
@@ -3,10 +3,9 @@ package com.github.skohar.homeip
 import com.amazonaws.services.lambda.runtime.Context
 
 class App {
-  def myHandler(myCount: Int, context: Context): String = {
+  def myHandler(myCount: String, context: Context): String = {
     val logger = context.getLogger
     logger.log("received : " + myCount)
-    logger.log("version : " + 1)
     String.valueOf(myCount)
   }
 }
