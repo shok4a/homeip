7101886769876f9d32029efe4f9455b1c62783d6
skohar <skohar@users.noreply.github.com>
2015-08-29 16:49:19 +0900

    .

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -x
+#!/bin/bash
 sbt assembly
 aws s3 cp target/scala-2.11/home-ip-assembly-0.1-SNAPSHOT.jar s3://homeip1/
 aws lambda update-function-code --region us-east-1 --function-name sample2 --s3-bucket homeip1 --s3-key home-ip-assembly-0.1-SNAPSHOT.jar
