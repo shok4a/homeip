3c229e9826d5c4d71afed494a60e915e868ef1bb
skohar <skohar@users.noreply.github.com>
2015-08-31 14:26:19 +0900

    .

--- src/test/scala/com/github/skohar/homeip/MySpec.scala
+++ src/test/scala/com/github/skohar/homeip/MySpec.scala
@@ -1,16 +1,9 @@
 package com.github.skohar.homeip
 
-import java.net.InetAddress
-
 import org.scalatest.FlatSpec
 
 class MySpec extends FlatSpec {
   "a" should "not equals to b" in {
     assert("a" !== "b")
-  }
-
-  "a" should "b" in {
-    new App().myHandler("192.168.0.1",null)
-    assert(1 === 1)
   }
 }
